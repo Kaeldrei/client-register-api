6db4ba5ecbb684334b49f9dc0d65ff0e53fd7b5b
Kaeldrei <mirandaengcompinfo@gmail.com>
2024-06-01 15:45:29 -0300

    Just changes

---
+++ src/test/java/com/example/client_register_api/ClientRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client_register_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
